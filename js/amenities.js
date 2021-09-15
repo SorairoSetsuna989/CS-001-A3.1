8c9baa6e05fa217732b649e26796037bb622d097
Jean Carlo San Juan <jcsanjuan12@gmail.com>
2021-09-15 21:04:55 +0800

    documentation

--- js/amenities.js
+++ js/amenities.js
@@ -13,14 +13,15 @@ const table = document.getElementById("areas");
 const views = document.getElementById("views");
 
 // Filename must match the area name
+// Uses a loop to spawn the elements
 for (let i = 0; i < areas.length; i+=2) {
     views.innerHTML += `
     <div class="area">
         ${areas[i]}
-        <img src="../media/${areas[i]}.jpg">
+        <img src="media/${areas[i]}.jpg">
     </div>
     <div class="area">
         ${areas[i+1]}
-        <img src="../media/${areas[i+1]}.jpg">
+        <img src="media/${areas[i+1]}.jpg">
     </div>`
 }
